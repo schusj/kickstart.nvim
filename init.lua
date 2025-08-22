bc5a67bf28ab192dbb6f3e94020cbf8a43ad691b
Jonathan Schuster <107444983+schusj@users.noreply.github.com>
2025-08-22 16:11:47 +1000

    changes

--- init.lua
+++ init.lua
@@ -665,21 +665,8 @@ require('lazy').setup({
               },
             },
           },
-          commands = {
-            OrganizeImports = {
-              function()
-                local params = {
-                  command = '_typescript.organizeImports',
-                  arguments = { vim.api.nvim_buf_get_name(0) },
-                  title = '',
-                }
-                vim.lsp.buf.execute_command(params)
-              end,
-              description = 'Organize Imports',
-            },
-          },
         },
-        volar = {},
+        vue_ls = {},
         terraformls = {},
         markdownlint = {},
         shellcheck = {},
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -26,11 +26,14 @@ return {
         delete = function(state)
           local inputs = require 'neo-tree.ui.inputs'
           local path = state.tree:get_node().path
+          -- split path into directory and file
+          local dir, file = path:match '(.*/)(.*)'
+
           local msg = 'Are you sure you want to trash ' .. path
           inputs.confirm(msg, function(confirmed)
             if not confirmed then return end
 
-            vim.fn.system { 'trash', vim.fn.fnameescape(path) }
+            vim.system({ 'trash', path }, {})
             require('neo-tree.sources.manager').refresh(state.name)
           end)
         end,
@@ -53,7 +56,7 @@ return {
           inputs.confirm(msg, function(confirmed)
             if not confirmed then return end
             for _, node in ipairs(selected_nodes) do
-              vim.fn.system { 'trash', vim.fn.fnameescape(node.path) }
+              vim.system { 'trash', node.path }
             end
             require('neo-tree.sources.manager').refresh(state.name)
           end)
